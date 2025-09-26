f322d4a72ebfe420c5b0a71ff5be9abef0e1f7c1
sufian2000168 <mdsufian1305@gmail.com>
2025-09-27 00:07:07 +0530

    fixed base url

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,36 @@ import './App.css'
 import Login from './Authentication/Login'
 import Codegenerator from './Home/Codegenerator'
 import { createTheme, ThemeProvider } from '@mui/material'
-import { BrowserRouter, Routes, Route ,Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
 const theme = createTheme({
-  typography:{
-    fontFamily:  `"proxima-nova", sans-serif`,
+  typography: {
+    fontFamily: `"proxima-nova", sans-serif`,
     fontWeightRegular: 400,
     fontWeightMedium: 500,
     fontWeightSemiBold: 600,
     fontWeightBold: 700,
   }
 })
+
 function App() {
 
   const PrivateRoute = ({ element }) => {
-  const token = localStorage.getItem('token');
-  return token ? element : <Navigate to="/login" />;
-};
+    const token = localStorage.getItem('token');
+    // Note: the path here should be relative to basename
+    return token ? element : <Navigate to="/login" replace />;
+  };
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/illustrata/embedsolution">
         <Routes>
+          {/* Use relative paths, not full paths */}
           <Route path="/" element={<PrivateRoute element={<Codegenerator />} />} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-      </ThemeProvider>
-    </>
+    </ThemeProvider>
   )
 }
 
