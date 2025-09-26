a71b5f114ab70e2d50ba17dfde435f6d81d52c6b
sufian2000168 <mdsufian1305@gmail.com>
2025-09-27 00:16:37 +0530

    fixed path

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/illustrata/embedsolution">
+      <BrowserRouter basename="/illustrata/embedsolution/">
         <Routes>
           {/* Use relative paths, not full paths */}
           <Route path="/" element={<PrivateRoute element={<Codegenerator />} />} />
