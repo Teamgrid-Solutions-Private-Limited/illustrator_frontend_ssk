bc0ac6f02d4959b6735d681b726a30922b6dcae4
Sakil7430 <sakil.hossain@teamgrid.io>
2025-09-27 16:35:42 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Set the base URL for production builds
   base: '/illustrata/embedsolution/',
 
   server: {
-    host: 'localhost', // Change to '0.0.0.0' for external access
+    host: 'localhost',
     port: 3001,
     fs: {
-      allow: ['..'] // Allows serving files from outside the project root
+      allow: ['..'] 
     }
   },
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Shortcut for imports from src
+      '@': path.resolve(__dirname, 'src'), 
     },
   },
 })
