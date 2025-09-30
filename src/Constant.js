bf7b6a9be862e58b9b6f6894a67040d9dd760b0e
Sakil7430 <sakil.hossain@teamgrid.io>
2025-09-30 11:48:43 +0530

    updated Contant file

--- src/Constant.js
+++ src/Constant.js
@@ -1,3 +1,2 @@
 export const BASE_API_URL = "https://demos.godigitalalchemy.com/illustrata/embed/backend/";
 export const EMBED_BASE_URL = "https://demos.godigitalalchemy.com/illustrata/embed";
-export const ILLUSTRATOR_WIDGET_URL = "https://illustrationinnovators.com/illustration";
